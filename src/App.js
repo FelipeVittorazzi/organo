b2c9e300544d69eca2710c5463378e0ab4880b2c
Felipe Martins Vittorazzi <felipe.vttzz@gmail.com>
2023-06-08 18:58:20 -0300

    == Organo Form + Enviando os dados do form como um objeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
 
 function App() {
+
+  const [collaborators, setCollaborators] = useState([]);
+
+  const onCollaboratorAdd = (collaborator) => {
+    console.log(collaborator)
+    setCollaborators([...collaborators, collaborator])
+  }
+
   return (
     <div className="App">
         <Banner />
-        <Form />
+        <Form onCollaboratorRegistered={collaborator => onCollaboratorAdd(collaborator)} />
     </div>
   );
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,7 +5,7 @@ import DropdownList from '../DropdownList';
 import TextField from '../TextField';
 import './Form.css';
 
-const Form = () => {
+const Form = (props) => {
 
     const times = [
         'Programação',
@@ -24,7 +24,12 @@ const Form = () => {
 
     const onSave = (event) => {
         event.preventDefault()
-        console.log('Form foi submetido => ' + nome, cargo, imagem, time)
+        props.onCollaboratorRegistered({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
 
     return (
